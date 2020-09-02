70ebca6547971387ab4c7bcf0a06e91ce397be15
751496032 <13480465265@126.com>
2020-09-02 17:20:27 +0800

    修改mate-data初始化检查方法

--- runtime/src/main/java/com/hzw/umeng/UMApiHelper.java
+++ runtime/src/main/java/com/hzw/umeng/UMApiHelper.java
@@ -18,7 +18,10 @@ import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.socialize.PlatformConfig;
 
+import org.json.JSONObject;
+
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -72,72 +75,99 @@ public class UMApiHelper {
 
     private static void checkConfiguration(Context context) {
         if (!Utils.isDebug) return;
-        try {
-            checkMetaData(context);
-            checkRegisterActivitys(context);
-            checkAnnotations(context);
+        checkMetaData(context);
+        checkRegisterActivitys(context);
+        checkAnnotations(context);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            Utils.error(e.getMessage());
-        }
     }
 
     private static void checkAnnotations(Context context) {
-        if (context instanceof  Application){
+        if (context instanceof Application) {
             Class<? extends Context> contextClass = context.getClass();
             WXBuilder annotation = contextClass.getAnnotation(WXBuilder.class);
-            if (annotation==null){
-                Utils.error(contextClass.getSimpleName()+"中缺少类注解：WXBuilder");
-            }else if (TextUtils.isEmpty(annotation.value())){
-                Utils.error("WXBuilder类注解中缺少value值："+context.getPackageName());
+            if (annotation == null) {
+                Utils.error(contextClass.getSimpleName() + "中缺少类注解：WXBuilder");
+            } else if (TextUtils.isEmpty(annotation.value())) {
+                Utils.error("WXBuilder类注解中缺少value值：" + context.getPackageName());
             }
 
         }
     }
 
-    private static void checkRegisterActivitys(Context context) throws PackageManager.NameNotFoundException {
-        PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES);
-        if (packageInfo.activities == null) return;
-        boolean isWXActivityRegistered = false, isWXPayActivityRegistered = false;
-        for (ActivityInfo activityInfo : packageInfo.activities) {
-            if (activityInfo == null || TextUtils.isEmpty(activityInfo.name)) continue;
-            if (activityInfo.name.contains(".wxapi.WXEntryActivity")) isWXActivityRegistered = true;
-            if (activityInfo.name.contains(".wxapi.WXPayEntryActivity")) isWXPayActivityRegistered = true;
-            if (isWXActivityRegistered && isWXPayActivityRegistered) break;
+    private static void checkRegisterActivitys(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES);
+            if (packageInfo.activities == null) return;
+            boolean isWXActivityRegistered = false, isWXPayActivityRegistered = false;
+            for (ActivityInfo activityInfo : packageInfo.activities) {
+                if (activityInfo == null || TextUtils.isEmpty(activityInfo.name)) continue;
+                if (activityInfo.name.contains(".wxapi.WXEntryActivity"))
+                    isWXActivityRegistered = true;
+                if (activityInfo.name.contains(".wxapi.WXPayEntryActivity"))
+                    isWXPayActivityRegistered = true;
+                if (isWXActivityRegistered && isWXPayActivityRegistered) break;
+            }
+            if (!isWXActivityRegistered) {
+                Utils.error("AndroidManifest中缺少：" + context.getPackageName() + ".wxapi.WXEntryActivity");
+            } else if (!isWXPayActivityRegistered) {
+                Utils.error("AndroidManifest中缺少：" + context.getPackageName() + ".wxapi.WXPayEntryActivity");
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
         }
-        if (!isWXActivityRegistered) {
-            Utils.error("AndroidManifest中缺少：" + context.getPackageName() + ".wxapi.WXEntryActivity");
-        }else if (!isWXPayActivityRegistered){
-            Utils.error("AndroidManifest中缺少：" + context.getPackageName() + ".wxapi.WXPayEntryActivity");
-        }
+
 
     }
 
 
-    private static void checkMetaData(Context context) throws PackageManager.NameNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
-        ApplicationInfo info = Utils.getApplicationInfo(context, PackageManager.GET_META_DATA);
-        Bundle bundle = info.metaData;
-        Class<?> superclass = bundle.getClass().getSuperclass();
-        if (superclass != null) {
-            Method method = superclass.getDeclaredMethod("getMap");
-            method.setAccessible(true);
-            Object o = method.invoke(bundle);
-            if (o instanceof ArrayMap) {
-                ArrayMap map = (ArrayMap) o;
-                Utils.debug(Utils.mapToJSON(map));
-                StringBuilder builder = new StringBuilder();
-                for (String key : metaDataKeys) {
-                    if (!map.containsKey(key)) {
-                        builder.append(key).append("\u3000");
-                    }
-                }
-                if (!TextUtils.isEmpty(builder)) {
-                    Utils.error("AndroidManifest中缺少缺少配置 meta-data ：" + builder.toString());
-                }
+    private static void checkMetaData(Context context) {
 
+        try {
+            ApplicationInfo info = Utils.getApplicationInfo(context, PackageManager.GET_META_DATA);
+            Bundle bundle = info.metaData;
+            //android 9.0 对 getMap添加了@hide，无法通过反射调用
+//            Class<?> superclass = bundle.getClass().getSuperclass();
+//            if (superclass != null) {
+//                Method method = superclass.getDeclaredMethod("getMap");
+//                method.setAccessible(true);
+//                Object o = method.invoke(bundle);
+//                if (o instanceof ArrayMap) {
+//                    ArrayMap map = (ArrayMap) o;
+//                    Utils.debug(Utils.mapToJSON(map));
+//                    StringBuilder builder = new StringBuilder();
+//                    for (String key : metaDataKeys) {
+//                        if (!map.containsKey(key)) {
+//                            builder.append(key).append("\u3000");
+//                        }
+//                    }
+//                    if (!TextUtils.isEmpty(builder)) {
+//                        Utils.error("AndroidManifest中缺少缺少配置 meta-data ：" + builder.toString());
+//                    }
+//
+//                }
+//            }
+
+            StringBuilder keys = new StringBuilder();
+            JSONObject jsonObject = new JSONObject();
+            for (String key : metaDataKeys) {
+                String value = bundle.getString(key);
+                if (TextUtils.isEmpty(value)) {
+                    keys.append(key).append("\u3000");
+                }else {
+                    jsonObject.put(key,value);
+                }
             }
+            if (!TextUtils.isEmpty(jsonObject.toString())){
+                Utils.debug(jsonObject.toString());
+            }
+            if (!TextUtils.isEmpty(keys)) {
+                Utils.error("AndroidManifest中缺少缺少配置 meta-data ：" + keys.toString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 
 
